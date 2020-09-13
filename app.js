43e64df47c701f94a15081974eb486f93f80f206
Shaheryar Shaikh <43124039+avc52github@users.noreply.github.com>
2020-09-13 19:35:55 +0530

    v3 added register route

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var User                  = require("./models/user");
 
 
 app.set("view engine","ejs");
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
 	secret:"Rusty is the best and cutest dog in the world",
 	resave: false,
@@ -24,9 +24,14 @@ app.use(require("express-session")({
 
 app.use(passport.initialize());
 app.use(passport.session());
- 
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+
+//================================================
+//Routes
+//================================================
 
 app.get("/",function(req,res) {
 
@@ -37,9 +42,27 @@ app.get("/",function(req,res) {
 app.get("/secret",function(req,res)
 {
 	res.render("secret");
-})
+});
 
 
+//show signup form
+app.get("/register",function(req,res) {
+	res.render("register");
+});
+
+//handling user signup
+app.post("/register",function(req,res) {
+	User.register(new User({username:req.body.username}),req.body.password,function(err,user) {
+		if(err)
+		{
+			console.log(err);
+			return res.render("register");
+		}	
+		passport.authenticate("local")(req,res,function() {
+			res.redirect("/secret");
+		})
+	});
+});
 
 app.listen(3000,'localhost',function() {
 	// body...
