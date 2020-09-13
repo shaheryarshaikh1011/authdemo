b30d0685993f248537a72382d61bb9d815087812
Shaheryar Shaikh <43124039+avc52github@users.noreply.github.com>
2020-09-13 19:00:20 +0530

    v2 added user model and configured passport

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# authdemo" 
+# authdemo
+* initial routes
+=========================
+# after that
+*create user model
+configure passport
+========================
+
+#routes
+*add register route
+*add register form
+
--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-var express = require("express");
-var mongoose = require("mongoose");
-
+var mongoose 			  = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/auth_demo_app");
+var express 			  = require("express");
+var passport 			  = require("passport");
+var bodyParser 			  = require("body-parser");
+var localStrategy 		  = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+
 var app     = express();
+
+var User                  = require("./models/user");
 
 
 app.set("view engine","ejs");
 
+app.use(require("express-session")({
+	secret:"Rusty is the best and cutest dog in the world",
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res) {
 
